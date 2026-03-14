ca85a1f65d2cb7b0035ff93374657d14e083ba12
Eduard <eduard_king_anterola@dlsl.edu.ph>
2026-03-14 23:21:23 +0800

    build: add configs/

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mctbwzqfdlvabghqcnlu.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
